21ac3f5de41d86afd26d5a2d1c5ba83f23b9407f
leewp-code <85806961+leewp-code@users.noreply.github.com>
2022-07-28 17:48:04 +0800

    Update download_model.sh

--- download_model.sh
+++ download_model.sh
@@ -3,7 +3,7 @@
 # md5sum 1219f5830e4a7208b1c7ba2f089a16c8
 
 FILENAME="remasternet.pth.tar"
-FILEURL="http://iizuka.cs.tsukuba.ac.jp/data/remasternet.pth.tar"
+FILEURL="k:\resources\github\video-restore\remasternet"
 FILEMD5="1219f5830e4a7208b1c7ba2f089a16c8"
 
 echo "Downloading the RemasterNet (245MB)..."
